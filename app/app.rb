d70085626470593bf681db30a0729269ce81a0b9
James Hughes <hugjimbo77@gmail.com>
2017-09-22 10:22:09 +0100

    multiple tags

--- app/app.rb
+++ app/app.rb
@@ -17,15 +17,24 @@ class BookMarkManager < Sinatra::Base
     erb :'links/new'
   end
 
+  # post '/links' do
+  #   link = Link.new(url: params[:url],
+  #                   title: params[:title])
+  #   tag = Tag.first_or_create(name: params[:tags]) #find first tag with name passed into params or if not found create tag with that name
+  #   link.tags << tag
+  #   link.save
+  #   redirect to('/links')
+  #   # Link.create(url: params[:url], title: params[:title])
+  # end
+
   post '/links' do
-    link = Link.new(url: params[:url],
-                    title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags]) #find first tag with name passed into params or if not found create tag with that name
-    link.tags << tag
-    link.save
-    redirect to('/links')
-    # Link.create(url: params[:url], title: params[:title])
-  end
+   link = Link.create(url: params[:url], title: params[:title])
+   params[:tags].split.each do |tag|
+     link.tags << Tag.first_or_create(name: tag)
+   end
+   link.save
+   redirect to('/links')
+ end
 
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
--- spec/features/adding_tags_spec.rb
+++ spec/features/adding_tags_spec.rb
@@ -1,13 +1,24 @@
 feature 'Adding tags' do
-  scenario 'I add a single tag to a new link' do
+  scenario 'I can add a single tag to a new link' do
     visit '/links/new'
     fill_in 'url', with: 'http://makersacademy.com/'  #1st box to fill in url
     fill_in 'title', with: 'Makers Academy' #2nd box to fill in title
     fill_in 'tags', with: 'education' #3rd box to fill in tag name
 
-    click_button 'Create link'  #actually creting the link with the above
+    click_button 'Create link'  #actually creating the link with the above
     link = Link.first #assigning instance of Link to variable
     expect(link.tags.map(&:name)).to include('education') #link.tags to form an array of tags(instance of tag class), the creating a new array of tag.names(tag names) and that array to include 'education'
+  end
+
+  scenario 'I can add multiple tags to a link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.bbc.co.uk/news'  #1st box to fill in url
+    fill_in 'title', with: 'BBC' #2nd box to fill in title
+    fill_in 'tags', with: 'education news sports'
+
+    click_button 'Create link'  #actually creating the link with the above
+    link = Link.first #assigning instance of Link to variable
+    expect(link.tags.map(&:name)).to include('education', 'news', 'sports') #link.tags to form an array of tags(instance of tag class), the creating a new array of tag.names(tag names) and that array to include 'education'
   end
 end
 
